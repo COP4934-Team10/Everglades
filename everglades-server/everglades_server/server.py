82470db964213889e874f6cb21dc15d5d0c540b6
Tarvalas <jamesdwyer1@live.com>
2020-01-28 16:41:19 -0500

    edited movement group ID reference

--- everglades-server/everglades_server/server.py
+++ everglades-server/everglades_server/server.py
@@ -767,7 +767,7 @@ class EvergladesGame:
                             outstr = '{:.6f},{},{},{},{},{}'.format(
                                     self.current_turn,
                                     player,
-                                    group.groupID,
+                                    group.mapGroupID,
                                     self.evgMap.nodes[start_idx].ID,
                                     self.evgMap.nodes[end_idx].ID,
                                     'ARRIVED'
