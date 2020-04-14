f55e79b5a4bfa8deca03c1759df28bd6536b29f8
Tarvalas <jamesdwyer1@live.com>
2020-04-14 10:31:53 -0400

    corrected some links in client codebase

--- test_battle.py
+++ test_battle.py
@@ -27,7 +27,7 @@ else:
 if len(sys.argv) > 3:
     map_name = sys.argv[3] + '.json'
 else:
-    map_name = 'DemoMap.json'
+    map_name = 'RandomMap.json'
 
 if map_name == 'RandomMap.json':
     generate_map.exec(7)
